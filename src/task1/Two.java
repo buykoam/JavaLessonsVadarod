dc8cfe4ed7bca1e61a966d78b7d034cb74f96d26
buykoam <anastasiya.buyko.teachmeskills@gmail.com>
2024-09-03 17:11:57 +0300

    solving second task

---
+++ src/task1/Two.java
@@ -0,0 +1,17 @@
+package task1;
+
+public class Two {
+    public static void main(String[] args) {
+        int x=5;
+        x--;
+        int y=2;
+        y++;
+        int z=6;
+        z--;
+        int c=x+y*z;
+        c++;
+        c++;
+        c++;
+        System.out.println(c);
+    }
+}
