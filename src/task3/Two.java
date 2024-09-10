a3ff8b851281d0d2b2835c1cc8981e8631bf206f
buykoam <anastasiya.buyko.teachmeskills@gmail.com>
2024-09-10 20:19:05 +0300

    solving  tasks

---
+++ src/task3/Two.java
@@ -0,0 +1,18 @@
+package task3;
+
+public class Two {
+    public static void main(String[] args) {
+        int num;
+        int num2;
+
+        num = 3;
+        num2 = 7;
+        for (int i = 1; i <= 100; i++) {
+            if ((i % num) == 0){
+                System.out.println(i);
+            } if ((i % num2)==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
