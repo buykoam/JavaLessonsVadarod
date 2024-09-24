753ebc5d315c017aec02c9fbbc5e81dae24d52c7
anastasiya.popkova <anastasiya.buyko.teachmeskills@gmail.com>
2024-09-24 17:46:52 +0300

    resolved task

---
+++ src/task8/StringDemo.java
@@ -0,0 +1,11 @@
+package task8;
+
+public class StringDemo {
+    public static void main(String[] args) {
+        CustomString customString = new CustomString("I like Java!!!");
+        customString.charAt();
+        customString.endsWith();
+        customString.startsWith();
+        customString.contains();
+    }
+}
