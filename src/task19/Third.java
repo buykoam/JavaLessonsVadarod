bda7d1ed325c5a996f46bb7ca500d73de31f97bd
buykoam <anastasiya.buyko.teachmeskills@gmail.com>
2024-11-03 18:27:37 +0300

    fix task19

--- src/task19/Third.java
+++ src/task19/Third.java
@@ -7,6 +7,6 @@ import java.time.format.DateTimeFormatter;
 public class Third {
     public static void main(String[] args) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd : hh:mm:ss a");
-        System.out.println(LocalDateTime.parse("2023-03-19 : 10:12:24 AM", formatter));
+        System.out.println(String.format("2023-03-19 : 10:12:24 AM", formatter));
     }
 }
